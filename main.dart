4ab8340033eea7e164fe1b3c11311f33c04c4ef3
Richaa <48063227+RICHAA22@users.noreply.github.com>
2020-03-08 10:25:57 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+void main()
+{
+runApp
+(
+MaterialApp
+(
+home: Scaffold
+(
+appBar: AppBar
+(
+backgroundColor: Colors.lightBlueAccent,
+title: Text('I am Rich!'),
+
+),
+body: Center
+( child: Image
+(image: AssetImage('images/jewel.png')),
+),
+),
+
+
+)
+
+
+
+);
+
+
+}
